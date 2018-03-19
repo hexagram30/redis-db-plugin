50a52caca75b0228e4cad9f6410b205897ea783b
Duncan McGreggor <oubiwann@gmail.com>
2018-03-18 22:09:47 -0500

    Added deps for graph API.

--- project.clj
+++ project.clj
@@ -24,6 +24,8 @@
     [clojurewerkz/ogre "3.3.1.0"]
     [clojusc/twig "0.3.2"]
     [com.orientechnologies/orientdb-core "2.2.33"]
+    [com.orientechnologies/orientdb-graphdb "2.2.33"]
+    [com.tinkerpop.blueprints/blueprints-core "2.6.0"]
     [hexagram30/common "0.1.0-SNAPSHOT"]
     [org.clojure/clojure "1.8.0"]
     [org.clojure/data.xml "0.0.8"]]
