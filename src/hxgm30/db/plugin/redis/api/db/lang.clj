fc04982cde74347837fb6cf6a77a7c2e5cb8a9f8
Duncan McGreggor <oubiwann@gmail.com>
2018-09-09 21:57:12 -0500

    Implemented new get-stats method for land db.

--- src/hxgm30/db/plugin/redis/api/db/lang.clj
+++ src/hxgm30/db/plugin/redis/api/db/lang.clj
@@ -25,10 +25,17 @@
   [this race-name name-type gen-type]
   (cmd this redis/get (schema/name-stats race-name name-type gen-type)))
 
+(defn -get-stats
+  ([this language gen-type]
+    (-lang-stats this this language gen-type))
+  ([this race-name name-type gen-type]
+    (-name-stats this this race-name name-type gen-type)))
+
 (def lang-behaviour
   {:ingest-stats -ingest-stats
    :lang-stats -lang-stats
-   :name-stats -name-stats})
+   :name-stats -name-stats
+   :get-stats -get-stats})
 
 (extend RedisLangDB
         DBAPI
